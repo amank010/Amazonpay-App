c7e8d2330c55fc4292ee96d785d44e8bb774d72a
amank010 <amanksingh010@gmail.com>
2025-05-06 22:37:35 +0530

    added

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+  console.log(`Visitor IP: ${ip}`);
+  res.send('Your visit has been logged.');
+});
+
+app.listen(3000, () => {
+  console.log('Server running on http://localhost:3000');
+});
